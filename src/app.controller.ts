bf195edfd255e1edf0fb6fb03d102a45771641e7
Anton.Prokharchyk <Anton.Prokharchyk@gmail.com>
2024-07-09 23:47:13 +0300

    add get animes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+
+type Anime = {
+  id: string;
+  name: string;
+  description: string;
+  img: string;
+  type: string;
+  episodes: string;
+  status: string;
+  genre: string;
+  source: string;
+  release: string;
+  studio: string;
+  'MPAA rank': string;
+  peggy: string;
+  duration: number;
+  dubbing: string;
+  sameAs: { id: string; name: string; img: string }[];
+};
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -10,7 +30,7 @@ export class AppController {
     return this.appService.getHello();
   }
   @Get()
-  getHello1(): string {
-    return this.appService.getHello();
+  getAnimes(): Anime[] {
+    return this.appService.getAnimes();
   }
 }
