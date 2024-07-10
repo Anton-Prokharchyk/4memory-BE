974157058a2bfdb76ed2422cfcce9e9d5cedfd4d
Anton.Prokharchyk <Anton.Prokharchyk@gmail.com>
2024-07-10 22:45:51 +0300

    wip

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -30,6 +30,10 @@ export class AppController {
     return this.appService.getHello();
   }
   @Get()
+  getHello1(): string {
+    return this.appService.getHello();
+  }
+  @Get()
   getAnimes(): Anime[] {
     return this.appService.getAnimes();
   }
